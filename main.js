d8913e52dda465444e2cb37610fed31ccac35a36
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2023-03-27 19:11:01 +0000

    updated gifts with create and append

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+const presentClosedUrl = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
+const presentOpenedUrl = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif'
 
 //get the image element on the screen
-const image = document.querySelector('img')
+const image = document.createElement('img');
+image.src = presentClosedUrl;
+
+//append this to the dom
+
+//select the title
+const title = document.querySelector('h1');
+
+document.body.appendChild(image);
 
 // add event listener to listen to mouse enter events
-image.addEventListener('mouseenter',  () => {
+image.addEventListener('click',  (event) => {
  
+  console.log(event);
+
   console.log("I am clicked, clickced !!")
-  image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif';
+  image.src = presentOpenedUrl;
+
+  title.classList.add("active")
+  title.classList.remove("inactive")
 
 } )
 
@@ -14,6 +29,23 @@ image.addEventListener('mouseenter',  () => {
 image.addEventListener('mouseleave',  () => {
  
   console.log("I am clicked, clickced !!")
-  image.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
-
+  image.src = presentClosedUrl
+  
+  title.classList.remove("active")
+  title.classList.add("inactive")
 } )
+
+
+let opened = false;
+setInterval(() => {
+  if(opened){
+    image.src = presentClosedUrl;
+  }else{
+    image.src = presentOpenedUrl;
+  }
+
+  opened = !opened;
+
+  console.log("opening", opened);
+
+}, 5000)
